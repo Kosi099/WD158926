9da327e65fb6b5384a2ad1f8ff3283bd19b68ca7
Kosi <kosiara099@wp.pl>
2020-04-02 12:17:06 +0200

    initial commit

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,6 +1,13 @@
+import random
 #Zadanie 1
 A = [1/x for x in range(1,11)]
 B = [2**x for x in range (11)]
 C = [x for x in B if x % 4 == 0]
 #Zadanie 2
+lista1 = [random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9)]
+lista2 = [random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9)]
+lista3 = [random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9)]
+lista4 = [random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9)]
+macierz = [lista1,lista2,lista3,lista4]
+przekatna = [lista1[0],lista2[1],lista3[2],lista4[3]]
 
