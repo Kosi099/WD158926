ddcc009fbdddd0401f7df11a75777417902c4bd3
Kosi <kosiara099@wp.pl>
2020-04-03 16:38:16 +0200

    initial commit

--- Zadanie1.py
+++ Zadanie1.py
@@ -10,4 +10,13 @@ lista3 = [random.randint(1,9), random.randint(1,9), random.randint(1,9), random.
 lista4 = [random.randint(1,9), random.randint(1,9), random.randint(1,9), random.randint(1,9)]
 macierz = [lista1,lista2,lista3,lista4]
 przekatna = [lista1[0],lista2[1],lista3[2],lista4[3]]
-
+#Zadanie 3
+skroty = {'Chleb':'sztuki','Ziemniaki':'kg','Papier_Toaletowy':'sztuki'}
+lista = []
+if skroty['Chleb']=="sztuki":
+    lista.append("Chleb")
+if skroty['Ziemniaki']=="sztuki":
+    lista.append("Ziemniaki")
+if skroty['Papier_Toaletowy']=="sztuki":
+    lista.append("Papier_Toaletowy")
+print(lista)
