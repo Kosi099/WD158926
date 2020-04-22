f933d7cd08507d67f840abe8956c8c15b55ebad9
Kosi <kosiara099@wp.pl>
2020-04-22 19:29:25 +0200

    initial commit

--- Lab5/zad8.py
+++ Lab5/zad8.py
@@ -8,7 +8,7 @@ class Samogloski:
 
     def __next__(self):
         for i in range(self.index):
-            if('self.data[self.index]=='a'' or 'self.data[self.index]=='e''or'self.data[self.index]=='i'' or 'self.data[self.index]=='o''or'self.data[self.index]=='u'' or 'self.data[self.index]=='y''):
+            if('self.data[self.index]==a'' or 'self.data[self.index]=='e''or'self.data[self.index]=='i'' or 'self.data[self.index]=='o''or'self.data[self.index]=='u'' or 'self.data[self.index]=='y''):
                 return self.data[self.index] 
         raise StopIteration
         
