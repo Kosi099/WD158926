75cd8e6bbb365478c83ee2fb1af956377de28b13
Kosi <kosiara099@wp.pl>
2020-04-22 19:26:25 +0200

    initial commit

---
+++ Lab5/zad8.py
@@ -0,0 +1,14 @@
+class Samogloski:
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        for i in range(self.index):
+            if('self.data[self.index]=='a'' or 'self.data[self.index]=='e''or'self.data[self.index]=='i'' or 'self.data[self.index]=='o''or'self.data[self.index]=='u'' or 'self.data[self.index]=='y''):
+                return self.data[self.index] 
+        raise StopIteration
+        
